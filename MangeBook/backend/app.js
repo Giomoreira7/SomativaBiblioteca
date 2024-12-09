d4f5a6837020f833e304f74ca8613f9eeb22d77d
nicolassmarangoni <gi.ovannamoreiradasilva2710@gmail.com>
2024-12-09 19:51:44 -0300

    G

--- MangeBook/backend/app.js
+++ MangeBook/backend/app.js
@@ -12,6 +12,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use (express.static(path.join(__dirname,'dist')))
+
+app.get('*',(req,res) =>{
+  res.sendFile(path.join(__dirname,'dist','index.html'));
+});
+
 // Configuração do multer para armazenar imagens
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
